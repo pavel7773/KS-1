3637684b276edf3870a92ce2c42ad9da02168d9e
ExpoZik <PashokFresh@yandex.ru>
2025-05-23 13:32:22 +0300

    KS-3-1 decision

---
+++ src/main/kotlin/lesson_1/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main() {
+    var greeting = "Добрый день"
+    val name = "Павел"
+    println("$greeting $name")
+    greeting = "Добрый вечер"
+    println("$greeting $name")
+}
