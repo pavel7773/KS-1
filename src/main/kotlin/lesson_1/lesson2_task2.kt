b90c7e5ef62ea73d287b73c23cd6e76907b137bd
ExpoZik <PashokFresh@yandex.ru>
2025-05-23 12:38:50 +0300

    KS-2-2 decision

---
+++ src/main/kotlin/lesson_1/lesson2_task2.kt
@@ -0,0 +1,20 @@
+package lesson_2
+
+fun main() {
+    // Kоличество сотрудников и их зарплата
+    val workers = 50
+    val salaryOfEmployees = 30000
+    // Kоличество стажеров и их зарплата
+    val interns = 30
+    val salaryOfInterns = 20000
+    // Расходы на выплату зарплаты постоянных сотрудников
+    val employeeExpenses = workers * salaryOfEmployees
+    // Общие расходы по ЗП после прихода стажеров
+    val generalExpenses = employeeExpenses + (interns * salaryOfInterns)
+    // Средняя ЗП одного сотрудника после устройства стажеров.
+    val totalEmployees  =  workers + interns
+    val averageSalary = generalExpenses /  totalEmployees
+    println("Расходы на выплату зарплаты постоянных сотрудников:$employeeExpenses")
+    println("Общие расходы по ЗП после прихода стажеров:$generalExpenses")
+    println("Средняя ЗП одного сотрудника после устройства стажеров:$averageSalary")
+}
