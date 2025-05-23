37edb78fbba3512921ca88ec19fa2d2e3412a323
ExpoZik <PashokFresh@yandex.ru>
2025-05-23 11:08:20 +0300

    KS-1-3 improvements

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,9 +2,11 @@ package lesson_3
 
 fun main() {
     val year = 1961
-    var hour = 9
-    hour = 10
-    var minute = 7
-    minute = 55
-    print("$hour:$minute")
+    var hour = 9.0
+    hour = 10.0
+    val intHour: Int = hour.toInt()
+    var minute = 7.0
+    minute = 55.0
+    val intMinute: Int = minute.toInt()
+    print("$intHour:$intMinute")
 }
