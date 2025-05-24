c2b79a0f760b30cdedebddcb613763bf82b6f4e0
ExpoZik <PashokFresh@yandex.ru>
2025-05-24 13:53:04 +0300

    KS-1-5 improvements

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,11 @@
 package lesson_5
-const val TOTAL_SECONDS = 6480
 const val SECONDS = 3600
 const val PERSENT = 60
 fun main() {
-
-    val hours = TOTAL_SECONDS / SECONDS
-    val minutes = (TOTAL_SECONDS % SECONDS) /PERSENT
-    val second = TOTAL_SECONDS % PERSENT
+    val totalSeconds = 6480
+    val hours = totalSeconds / SECONDS
+    val minutes = (totalSeconds % SECONDS) /PERSENT
+    val second = totalSeconds % PERSENT
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, second)
     println(formattedTime)
 }
